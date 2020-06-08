08f305bde79524d3b6157b75c89d4c3e894bd590
Muhammad Shoaib <shoaib.hafiz37@gmail.com>
2020-06-08 17:17:45 +0500

    update

--- LandAreaPrediction/settings.py
+++ LandAreaPrediction/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'z4b#$z70f345-7imln!8q71e76l4-e2fs!@=a$+l0iswuuc3_='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['landvalueanalyzer.herokuapp.com']
 
 
 # Application definition
@@ -128,3 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media'
