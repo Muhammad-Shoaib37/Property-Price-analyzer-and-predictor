0af08fcffbacaf9e0cf26405fdc984287d9eadf5
Muhammad Shoaib <shoaib.hafiz37@gmail.com>
2020-06-08 17:50:56 +0500

    creating settings.py

--- LandAreaPrediction/settings.py
+++ LandAreaPrediction/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -134,3 +135,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media'
+
+django_heroku.settings(locals())
